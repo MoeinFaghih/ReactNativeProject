414c04849ef902c54a6f6607dec1cc7318dd5535
MoeinFaghih <momeix.f78@gmail.com>
2024-12-25 22:10:30 +0300

    Created babel.config.js/

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,17 @@
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import {  Link } from 'expo-router';
+
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text1}>Sarvin Golchin!</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl">Sarvin Golchin!</Text>
       <StatusBar style="auto"/>
       <Link href="/profile" style={{color: 'blue'}}>Go To Profile</Link>
-      <Text style={styles.text2}>Developmental Psychologist</Text>
+      <Text >Developmental Psychologist</Text>
       
 
     </View>
@@ -19,20 +20,3 @@ export default function App() {
 }
 
 
-const styles = StyleSheet.create ({
-  container:{
-    backgroundColor: '#e6ffff',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    
-  },
-  text1:{
-    fontSize: 80,
-    textAlign: 'center',
-  },
-  text2:{
-    fontSize: 30,
-    marginTop: 85,
-  }
-})
